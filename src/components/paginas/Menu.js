c4e8991d982e4fcd0e65da8a5efaa98b0e5db546
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2021-03-28 18:25:27 -0600

    Componente Menu de restaurante, operaciones de lectura sobre firebase

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase';
+
+import Platillo from '../ui/Platillo';
+
 
 const Menu = () => {
+    // definir el state para los platillos
+    const [platillos, guardarPlatillos] = useState([]);
+
+    const { firebase } = useContext(FirebaseContext);
+
+    // consultar la base
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+            firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+        }
+        obtenerPlatillos();
+    }, []);
+
+    function manejarSnapshot(snapshot){
+        const platillos = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+        guardarPlatillos(platillos)
+    }
+
     return ( 
         <>
         <h1 className="text-3xl font-light mb-4">Menu</h1>
 
         <Link to="/nuevo-platillo" className="bg-blue-800 hover:bg-blue-700 inline-block mb-5 p-2 text-white uppercase font-bold">
             Agregar Platillo
-        </Link>        
+        </Link>  
+
+        {platillos.map(platillo => (
+            <Platillo 
+                key={platillo.id}
+                platillo={platillo}
+            />
+        ))}      
         </>
      );
 }
